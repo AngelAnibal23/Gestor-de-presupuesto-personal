431dfafef130626e0ff675211bb8105fa1aee827
Guts <aamamanica@unjbg.edu.pe>
2025-02-23 21:26:56 -0500

    Agregando el menu provisional

--- main.cpp
+++ main.cpp
@@ -3,7 +3,48 @@
 
 int main() {
 	
-	//project has just started 2/15/2025 
 	
+	do{
+		int opcion; 	
+	
+			cout << "------------GESTOR DE FINANZAS PERSONAL------------" << endl; 
+			cout << " [1] Mostrar registro general. " << endl; 
+			cout << " [2] Registrar ingresos o gastos. " << endl; 
+			cout << " [3] Modificar algun monto. " << endl; 
+			cout << " [4] Filtrar por fecha. " << endl; 
+			cout << " [5] Filtrar por tipo de monto." << endl; 
+			cin >> opcion; 
+			
+			switch(opcion){
+				case 1: {
+				
+					break;
+				}
+				case 2: {
+					
+					break;
+				}
+				case 3: {
+					
+					break;
+				}
+				case 4:{
+					
+					break;
+				} 
+				case 5: {
+					
+					break;
+				}
+				default: {
+					err << "Opcion no valida, vuelva a ingresar" << endl; 
+					break;
+				}
+			}
+		
+		
+	}while(opcion != 0); 
+	
+		
 	return 0;
 }
